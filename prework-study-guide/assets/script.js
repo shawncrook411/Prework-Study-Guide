e3f7974ed5b68009046124a31acefbd0d8710a2c
shawncrook411 <shawncrook411@gmail.com>
2023-08-12 15:59:51 -0600

    Added Javascript for loop that prints different topics to the console log

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,33 @@
+/*(var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+}
+else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+}
+else if (topic === 'Git') {
+    console.log("Let's study Git!");
+}
+else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript");
+}
+else {
+    console.log('Please try again!');
+}
+*/
+
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(x = 0; x < topic.length; x++) {
+    console.log(topic[x]);
+}
+
+/*var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);   
+}
+*/
+
